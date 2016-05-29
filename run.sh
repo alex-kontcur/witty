504d41d7c058611788784db68824903e04ae730c
alexander-k <kontsur-a@yandex.ru>
2016-05-29 08:22:09 +0400

    [*] run script

---
+++ run.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+#
+# command line runner for the witty servers
+#
+
+function cleanup() {
+    kill ${CLIENT_PID} ${SERVER_PID}
+}
+
+trap cleanup EXIT
+
+mvn clean package
+java -jar consumer/target/consumer-1.0.0.jar &
+SERVER_PID=$!
+
+while ! nc localhost 8080 > /dev/null 2>&1 < /dev/null; do
+    echo "$(date) - waiting for server at localhost:8080..."
+    sleep 1
+done
+
+java -jar producer/target/producer-1.0.0.jar &
+CLIENT_PID=$!
+
+echo "For EXIT press 'q'"
+
+while :
+do
+    read -t 1 -n 1 key
+
+    if [[ $key = q ]]
+    then
+        break
+    fi
+done
+
+cleanup
